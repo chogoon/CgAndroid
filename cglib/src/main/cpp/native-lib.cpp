72b21ca3b3058f4bce91e2fd3d7f552ed407526e
chogoon <jakssal79@gmail.com>
2020-02-07 16:09:54 +0900

    cmake 테스트2

--- cglib/src/main/cpp/native-lib.cpp
+++ cglib/src/main/cpp/native-lib.cpp
@@ -1,10 +1,10 @@
-#include <jni.h>
-#include <string>
-//com.chogoon.cglib
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_chogoon_cglib_BaseActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
-}
+//#include <jni.h>
+//#include <string>
+////com.chogoon.cglib
+//extern "C" JNIEXPORT jstring JNICALL
+//Java_com_chogoon_cglib_BaseActivity_stringFromJNI(
+//        JNIEnv *env,
+//        jobject /* this */) {
+//    std::string hello = "Hello from C++";
+//    return env->NewStringUTF(hello.c_str());
+//}
